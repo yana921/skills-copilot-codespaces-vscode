2fd200137592e2de48f60d343371e000704dd0c5
yana921 <118464159+yana921@users.noreply.github.com>
2024-09-30 16:16:39 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,25 @@
+// create web server
+// node comments.js
+// http://localhost:3000
+// http://localhost:3000/comments
+
+// create web server
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(req, res){
+  console.log('request was made: ' + req.url);
+  if(req.url === '/comments' || req.url === '/'){
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    var comments = {
+      user: 'John',
+      message: 'Hello World'
+    };
+    res.end(JSON.stringify(comments));
+  } else {
+    res.writeHead(404, {'Content-Type': 'text/html'});
+    fs.createReadStream(__dirname + '/404.html').pipe(res);
+  }
+});
+
+server.listen(3000, '
